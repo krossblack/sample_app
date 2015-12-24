7053862e6e4231f002447112fa70f2da9de624c2
tjinjin <tjinjinprogram@gmail.com>
2015-12-24 22:47:32 +0900

    Access http

--- config/deploy/vm.rb
+++ config/deploy/vm.rb
@@ -1,12 +1,11 @@
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 set :url, 'http://localhost/'
-
+set :repo_url, 'https://github.com/krossblack/sample_app'
 set :ssh_options, {
-
    user: 'vagrant',
    keys: %w(~/.vagrant.d/insecure_private_key),
-   forward_agent: false,
+   forward_agent: true,
    auth_methods: %w(publickey)
 }
 
