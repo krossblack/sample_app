b611d24c99f59802aaf65f707f056a212f65859c
tjinjin <tjinjinprogram@gmail.com>
2015-12-28 01:23:52 +0900

    Install postgresql

--- chef/site-cookbooks/rails/recipes/default.rb
+++ chef/site-cookbooks/rails/recipes/default.rb
@@ -9,3 +9,21 @@
 directory '/var/' do
   mode '0777'
 end
+
+
+#yum_repository "postgresql" do
+#  url "http://yum.postgresql.org/9.4/redhat/rhel-7-x86_64/pgdg-redhat94-9.4-2.noarch.rpm"
+#  action :add
+#end
+#
+
+package_rpm = 'pgdg-redhat94-9.4-2.noarch.rpm'
+remote_file "/tmp/#{package_rpm}" do
+  source "http://yum.postgresql.org/9.4/redhat/rhel-7-x86_64/#{package_rpm}"
+end
+
+rpm_package "#{package_rpm}" do
+  source "/tmp/#{package_rpm}"
+end
+
+package 'postgresql-devel'
