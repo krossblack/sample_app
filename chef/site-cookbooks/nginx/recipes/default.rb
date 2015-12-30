94c5c2d2a02de7e7c0c9b54e4bb87012a9c3158a
tjinjin <tjinjinprogram@gmail.com>
2015-12-31 03:27:39 +0900

    Install nginx

---
+++ chef/site-cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: nginx
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe 'yum-epel'
+
+package 'nginx'
+
+service 'nginx' do
+  action [:enable, :start]
+  supports reload: true
+end
+
+%w(default.conf example_ssl.conf).each do |file|
+  file "/etc/nginx/conf.d/#{file}" do
+    action :delete
+    notifies :reload, 'service[nginx]'
+  end
+end
